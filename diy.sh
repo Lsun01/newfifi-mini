13dae2229c0f4cf934f777e53a7b2d40542fe354
Lsun01 <xroot009@icloud.com>
2020-03-28 22:34:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,3 +8,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sudo apt-get install gcc-multilib g++-multilib
+find ./bin/targets/ -type d -name "packages" | xargs rm -rf {}
