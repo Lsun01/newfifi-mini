e6dccf0737a0af80f2c9cd93f2ca5b35f4266504
Lsun01 <xroot009@icloud.com>
2020-03-30 00:25:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,5 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/Lienol/openwrt-package package/openwrt-package
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/tty228/luci-app-serverchan package/serverchan
 sudo apt-get install gcc-multilib g++-multilib
-find ./bin/targets/ -type d -name "packages" | xargs rm -rf {}
